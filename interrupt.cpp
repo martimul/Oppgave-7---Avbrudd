bb2ca71e5bdeac1abe14cc4c4d311a19dc0183e5
martimul <hansmartinbarnes@gmail.com>
2018-11-12 10:30:38 +0100

    Update interrupt.cpp

--- interrupt.cpp
+++ interrupt.cpp
@@ -18,21 +18,21 @@ int main(void)
 	DDRD &= ~(1 << DDD2);		// Clearer PD2 pinnen og setter PD2(INT0) som input
 
 
-	PORTD = (1 << PD2);			// Slår på PD2(INT0) som input med pull-up resistor aktivert
+	PORTD = (1 << PD2);			// SlÃ¥r pÃ¥ PD2(INT0) som input med pull-up resistor aktivert
 	
 	DDRB  = (1 << PB1);			// Setter PB1 som input
 	
 	
-	EICRA |= (1 << ISC01);      // Setter INT0 til å "triggre" ved en endring, feks en knapp blir trykket. 
-	EIMSK |= (1 << INT0);       // Slår på INT0
+	EICRA |= (1 << ISC01);      // Setter INT0 til Ã¥ "triggre" ved en endring, feks en knapp blir trykket. 
+	EIMSK |= (1 << INT0);       // SlÃ¥r pÃ¥ INT0
 
 
 	
 	while(1)
 	{
-		if(cliFlag == 0)	// cliFlag er en variabel som hvis lik 0, brukes til å sette sei();, som betyr set interrupt. 
-							// Hvis den er lik 1 brukes den til å sette cli();, som betyr clear interrupt.
-							// Hvis 
+		if(cliFlag == 0)	// cliFlag er en variabel som hvis lik 0, brukes til Ã¥ sette sei();, som betyr set interrupt. 
+							// Hvis den er lik 1 brukes den til Ã¥ sette cli();, som betyr clear interrupt.
+						
 		{
 			sei();
 		}
